8980c4825cdeabf2112550793d1bb743714b0880
chunghyun lee <sinna94@naver.com>
2023-01-16 23:40:35 +0900

    etc: swagger 문서 추가

---
+++ src/main/kotlin/me/chung/ecommerceapi/config/OpenApiConfig.kt
@@ -0,0 +1,18 @@
+package me.chung.ecommerceapi.config
+
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class OpenApiConfig {
+
+  @Bean
+  fun customOpenApi(): OpenAPI {
+    return OpenAPI().info(
+      Info()
+        .title("e-commerce API 문서")
+    )
+  }
+}
--- src/main/kotlin/me/chung/ecommerceapi/config/SecurityConfig.kt
+++ src/main/kotlin/me/chung/ecommerceapi/config/SecurityConfig.kt
@@ -2,29 +2,28 @@ package me.chung.ecommerceapi.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 
-@EnableWebSecurity
 @Configuration
 class SecurityConfig {
-
-//    @Bean
-//    fun configure(): WebSecurityCustomizer {
-//        return WebSecurityCustomizer { web ->
-//            web.ignoring()
-//                .requestMatchers(HttpMethod.GET, "/api/v/user/signup")
-//        }
-//    }
 
   @Bean
   fun filterChain(httpSecurity: HttpSecurity): SecurityFilterChain {
     return httpSecurity.csrf()
       .disable()
       .authorizeHttpRequests { auth ->
-        auth.requestMatchers("/v1/user/signup").permitAll()
+        auth
+          .requestMatchers(
+            HttpMethod.GET,
+            "/swagger-ui/**", "/v3/api-docs/**"
+          ).permitAll()
+          .requestMatchers(
+            HttpMethod.POST,
+            "/v1/user/signup",
+          ).permitAll()
           .anyRequest().authenticated()
       }.build()
   }
